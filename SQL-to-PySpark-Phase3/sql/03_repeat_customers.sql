fc60a57c09b2c45c247d5de99de9e49accbdcb76
bathula-sushmareddy <bathulasushma46@gmail.com>
2026-07-08 22:28:48 +0530

    Update 03_repeat_customers.sql

--- SQL-to-PySpark-Phase3/sql/03_repeat_customers.sql
+++ SQL-to-PySpark-Phase3/sql/03_repeat_customers.sql
@@ -10,8 +10,3 @@ GROUP BY customer_id
 HAVING COUNT(*) > 2
 """)
 result.show()
-print("===== Repeat Customers (PySpark) =====")
-result = sales.groupBy("customer_id") \
-              .agg(count("*").alias("orders")) \
-              .filter(col("orders") > 2)
-result.show()
