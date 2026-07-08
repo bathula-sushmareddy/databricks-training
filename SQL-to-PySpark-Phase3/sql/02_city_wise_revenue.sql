3ad34afe573b779148fda4e655899dab522e59e6
bathula-sushmareddy <bathulasushma46@gmail.com>
2026-07-08 22:27:44 +0530

    Update 02_city_wise_revenue.sql

--- SQL-to-PySpark-Phase3/sql/02_city_wise_revenue.sql
+++ SQL-to-PySpark-Phase3/sql/02_city_wise_revenue.sql
@@ -12,9 +12,4 @@ ON c.customer_id = s.customer_id
 GROUP BY c.city
 """)
 result.show()
-print("===== City-wise Revenue (PySpark) =====")
 
-result = customers.join(sales, "customer_id") \
-                  .groupBy("city") \
-                  .agg(sum("total_amount").alias("revenue"))
-result.show()
